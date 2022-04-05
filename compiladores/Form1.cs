143f293931850b80f4e0e794a61c793f3204ebc8
Renan Skonicezny Vilela <renansko@gmail.com>
2022-04-05 12:33:58 -0300

    comentarios

--- compiladores/Form1.cs
+++ compiladores/Form1.cs
@@ -17,51 +17,49 @@ namespace compiladores
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            char[] arr;
-            int start = 1;
-            string text = "";
+            
+            char[] arr; // array de char
+            int start = 1; //Começo da leitura de string
+            string text = ""; // texto para o windowns forms
 
-            if (textBox1.Text == "")
+            if (textBox1.Text == "") // verifica se foi digitado algo na textBox
             {
                 MessageBox.Show("é necessario digitar o token!");
                 textBox1.Focus();
                 return;
             }
 
-            arr = textBox1.Text.ToCharArray();
-            Array myArray = Array.CreateInstance(typeof(string), arr.Length);
+            arr = textBox1.Text.ToCharArray(); // resgata o texto da textBox e transforma ele em uma Array de CHAR[]
 
-            if (arr[0].ToString() != "=")
+            if (arr[0].ToString() != "=") // verifica se o texto começa com =
             {
                 MessageBox.Show("Deve digitar o = no começo da equação");
                 return;
             }
             
-            for (int k = 0; k < arr.Length; k++)
+            for (int k = 0; k < arr.Length; k++) // percorre todos elementros do array char para achar uma condição
             {
                 if(k == arr.Length - 1)
                 {
                     continue;
                 }
-                if (Char.IsNumber(arr[k]) && Char.IsLetter(arr[k + 1]))
+                if (Char.IsNumber(arr[k]) && Char.IsLetter(arr[k + 1])) // caso tenha um numero seguido por uma letra era gerar um erro
                 {
                     throw new Exception("Formato errado, nao eh possivel ler numero e letra em sequencia");
                 }
             }
 
-            for (int i = 1; i < arr.Length; i++)
+            for (int i = 1; i < arr.Length; i++) // percorre todos elementos da array Char
             {
-                if (Char.IsSymbol(arr[i]) || (!Char.IsDigit(arr[i]) && !Char.IsLetter(arr[i])))
+                if (Char.IsSymbol(arr[i]) || (!Char.IsDigit(arr[i]) && !Char.IsLetter(arr[i])))// identifica quando existir um sibolo
                 {
-                    for (int j = start; j < i; j++)
+                    for (int j = start; j < i; j++) // percorre todos elementos entre simbolos
                     {
-                        string String = String.Join("", arr[j]);
-                        myArray.SetValue(String, j-1);
-         
-                        text = text + myArray.GetValue(j-1).ToString();
+                        string String = String.Join("", arr[j]);// pega o char e o transforma em um STRING
+                        text = text + String; // junta os caracteres em um texto
                     }
                  
-                    if (Char.IsLetter(text[0]))
+                    if (Char.IsLetter(text[0])) // verifica se o primeiro caracter é uma letra
                     {
                         string[] variavel = new string[4000];
 
@@ -74,7 +72,7 @@ namespace compiladores
                         start = i+1;
                     }
                         
-                    if (Char.IsDigit(text[0]))
+                    if (Char.IsDigit(text[0])) // verifica se o primeiro caracter é um numero
                     {
                         string[] variavel = new string[4000];
 
@@ -86,7 +84,7 @@ namespace compiladores
                         listView1.Items.Add(v);
                         start = i +1;
                     }
-                        string tipo = verifySimbols(arr[i]);
+                        string tipo = verifySimbols(arr[i]); // insere o simbolo na tabela
                         string[] tk = new string[4000];
 
                         tk[0] = arr[i].ToString(); // valor
@@ -101,7 +99,7 @@ namespace compiladores
             }
         }
 
-        private string verifySimbols(char simbolo)
+        private string verifySimbols(char simbolo) // verifica qual é o simbolo
         { 
             string tipo = string.Empty;
             if(simbolo == '+')
